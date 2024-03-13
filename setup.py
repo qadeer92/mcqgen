d1a7cc35b09f3f81fd516f908e72cdac80499b27
Qadeer Ahmed <qadeer92@hotmail.com>
2024-03-14 00:15:34 +0300

    Files updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgenerator',
+    version='0.0.1',
+    author='Qadeer Ahmed',
+    author_email='qadeer92@hotmail.com',
+    insall_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+)
